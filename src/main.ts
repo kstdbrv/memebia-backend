898cda7f918eac5da94c366789a99900fdcbb71d
Ilya Kurganskiy <kurganskiy@mercurydevelopment.com>
2021-07-26 00:01:09 +0400

    feat(swagger): add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+import {
+  ConfigurationVariables,
+  SwaggerConfig,
+} from '@config/configuration.model';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService: ConfigService<ConfigurationVariables> =
+    app.get(ConfigService);
+  const swaggerConfig = configService.get<SwaggerConfig>('swagger');
+
+  // Swagger Api
+  if (swaggerConfig.enabled) {
+    const options = new DocumentBuilder()
+      .setTitle(swaggerConfig.title)
+      .setDescription(swaggerConfig.description)
+      .setVersion(swaggerConfig.version)
+      .addBearerAuth()
+      .build();
+    const document = SwaggerModule.createDocument(app, options);
+
+    SwaggerModule.setup(swaggerConfig.path, app, document);
+  }
+
   await app.listen(3000);
 }
 bootstrap();
